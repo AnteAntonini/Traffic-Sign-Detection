4f53b014745199a77f286e439f8af48aacc1dd3c
AnteAntonini <anteantonini.mtk@hotmail.com>
2022-04-28 20:18:44 +0200

    create neural network the functional way

--- example.py
+++ example.py
@@ -24,6 +24,23 @@ model = tensorflow.keras.Sequential(
     ]
  )
 
+ # functional way
+def functional_model():
+    
+    my_input = Input(shape=(28,28,1)),
+    x = Conv2D(32, (3,3), activation='relu')(my_input)
+    x = Conv2D(64, (3,3), activation='relu')(x)
+    x = MaxPool2D()(x)
+    x = BatchNormalization()(x)
+ 
+    x = Conv2D(128, (3,3), activation='relu')(x)
+    x = MaxPool2D()(x)
+    x = BatchNormalization()(x)
+ 
+    x = GlobalAvgPool2D()(x)
+    x = Dense(64, activation='relu')(x)
+    x = Dense(10, activation='softmax')(x)     
+
 if __name__ == '__main__':
     
     (x_train, y_train), (x_test, y_test) = tensorflow.keras.datasets.mnist.load_data()
