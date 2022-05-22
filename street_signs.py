bcdc9755f54073978502daead5e142c78ac0de99
AnteAntonini <anteantonini.mtk@hotmail.com>
2022-05-22 23:02:34 +0200

    add image and csv file, import test_set function

--- street_signs.py
+++ street_signs.py
@@ -3,14 +3,19 @@ import glob
 import shutil
 from sklearn.model_selection import train_test_split
 
-from utils import split_data
+from utils import split_data, order_test_set
 
 if __name__ == "__main__":
 
-    data_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\Train"
-    save_train_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\training_data\\train"
-    save_val_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\training_data\\val"
-    split_data(data_path, save_train_path = save_train_path, save_val_path = save_val_path)
+    if False:
+        data_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\Train"
+        save_train_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\training_data\\train"
+        save_val_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\training_data\\val"
+        split_data(data_path, save_train_path = save_train_path, save_val_path = save_val_path)
+    
+    images_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\Test"
+    csv_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\Test.csv"
+    order_test_set(images_path, csv_path)
 
 
 
