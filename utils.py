ca5cf3ee7ef6618144fe2df882e38c7a023302a6
AnteAntonini <anteantonini.mtk@hotmail.com>
2022-05-22 22:50:46 +0200

    clean up code, move split data function to utils

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+import glob
+import shutil
+from sklearn.model_selection import train_test_split
 
 
 
@@ -19,3 +23,32 @@ def display_some_examples(examples, labels):
         plt.imshow(img, cmap='gray')
 
     plt.show()
+
+def split_data(data_path, save_train_path, save_val_path, split_size=0.1):
+
+    folders = os.listdir(data_path)
+
+    for folder in folders:
+        
+        full_path = os.path.join(data_path, folder)
+        images_paths = glob.glob(os.path.join(full_path, '*.png'))
+
+        x_train, x_val = train_test_split(images_paths, test_size=split_size)
+
+        for x in x_train:
+
+            path_to_folder = os.path.join(save_train_path, folder)
+
+            if not os.path.isdir(path_to_folder):
+                os.makedirs(path_to_folder)
+
+            shutil.copy(x, path_to_folder)
+
+        for x in x_val:
+
+            path_to_folder = os.path.join(save_val_path, folder)
+
+            if not os.path.isdir(path_to_folder):
+                os.makedirs(path_to_folder)
+
+            shutil.copy(x, path_to_folder)
