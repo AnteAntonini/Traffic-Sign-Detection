5147edec83111358f0037a8f711a6328a626f092
AnteAntonini <anteantonini.mtk@hotmail.com>
2022-05-28 16:02:16 +0200

    evaluate model

--- street_signs.py
+++ street_signs.py
@@ -8,7 +8,7 @@ from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 
 from utils import split_data, order_test_set, create_generators
 
-from models import streetsigns_model
+import tensorflow as tf
 
 if __name__ == "__main__":
 
@@ -32,21 +32,35 @@ if __name__ == "__main__":
     train_generator, val_generator, test_generator  = create_generators(batch_size,train_path, val_path, test_path)
     num_classes = train_generator.num_classes
 
-    save_model_path = './Models'
-    checkpoint_saver = ModelCheckpoint(
-        save_model_path,
-        monitor="val_accuracy",
-        mode='max',
-        save_best_only=True,
-        save_freq='epoch',
-        verbose=1
-    )
+    TRAIN=False
+    TEST=True
 
-    early_stop = EarlyStopping(monitor="val_accuracy", patience=10)
+    if TRAIN:
+        save_model_path = './Models'
+        checkpoint_saver = ModelCheckpoint(
+            save_model_path,
+            monitor="val_accuracy",
+            mode='max',
+            save_best_only=True,
+            save_freq='epoch',
+            verbose=1
+        )
 
-    model = streetsigns_model(num_classes)
+        early_stop = EarlyStopping(monitor="val_accuracy", patience=10)
 
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        model = streetsigns_model(num_classes)
 
-    model.fit(train_generator,epochs=epochs,batch_size=batch_size,validation_data=val_generator, callbacks=[checkpoint_saver, early_stop])
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+        model.fit(train_generator,epochs=epochs,batch_size=batch_size,validation_data=val_generator, callbacks=[checkpoint_saver, early_stop])
+
+    if TEST:
+        model = tf.keras.models.load_model('./Models')
+        model.summary()
+
+        print("Evaluating validation set:")
+        model.evaluate(val_generator)
+
+        print("Evaluating test set : ")
+        model.evaluate(test_generator)
 
