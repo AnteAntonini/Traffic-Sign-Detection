27b33e870df8071f46f308b64f0abd259423e317
AnteAntonini <anteantonini.mtk@hotmail.com>
2022-05-28 15:07:46 +0200

    instantiate generators

--- street_signs.py
+++ street_signs.py
@@ -3,7 +3,7 @@ import glob
 import shutil
 from sklearn.model_selection import train_test_split
 
-from utils import split_data, order_test_set
+from utils import split_data, order_test_set, create_generators
 
 from models import streetsigns_model
 
@@ -11,13 +11,21 @@ if __name__ == "__main__":
 
     if False:
         data_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\Train"
-        save_train_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\training_data\\train"
-        save_val_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\training_data\\val"
-        split_data(data_path, save_train_path = save_train_path, save_val_path = save_val_path)
+        train_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\training_data\\train"
+        val_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\training_data\\val"
+        split_data(data_path, train_path = train_path, val_path = val_path)
     
-    images_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\Test"
-    csv_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\Test.csv"
-    order_test_set(images_path, csv_path)
+    if False:
+        images_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\Test"
+        csv_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\Test.csv"
+        order_test_set(images_path, csv_path)
 
+    train_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\training_data\\train"
+    val_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\training_data\\val"    
+    test_path = "C:\\Users\\niniy\\Downloads\\TrafficSigns\\Test"
+    batch_size = 32
 
+    train_generator, val_generator, test_generator  = create_generators(batch_size,train_path, val_path, test_path)
+
+    
 
